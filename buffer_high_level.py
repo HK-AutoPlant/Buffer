4e1f2c50cf531fff203e79d76405e0d76c45bd26
Oskar Nordström <oskar.norden.kth@gmail.com>
2020-10-29 16:51:49 +0100

    cHANGES

--- buffer_high_level.py
+++ buffer_high_level.py
@@ -6,7 +6,7 @@ import signal
 import sys
 import threading
 import RPi.GPIO as GPIO
-
+import logging
 """
 HK2020 Autoplant 1 - Buffer Subsystem
 This is the high level software main file for the 2020 Mechatronics HK - Autoplant 1
@@ -16,77 +16,9 @@ Microcontrollers (Arduino Nanos) which in turn control the hardware.
 This file has following classes:
 MainBuffer
 NanoUsb(port(optional, preset to /dev/ttyUSB0), baud(optional, preset to 115200)
+https://roboticsbackend.com/raspberry-pi-gpio-interrupts-tutorial/
+
 """
-
-class EmergencyInterruptException(Exception):
-    print("Emergency Sensor Triggered")
-    pass
-
-class BufferThread(threading.Thread):
-    """
-    This class deals with overall workings and logics for the buffer system
-    Call this class to create one thread for one buffer tray system
-    Sends the "init" string to Arduino tied to thread
-    Arduino handling
-    """
-    def __init__(self, arduino_port, thread_id, thread_counter):
-        """
-
-        :param arduino_port: Insert the
-        """
-        # Init thread params, open connection to arduino tied to this thread
-        threading.Thread.__init__(self)
-        self.Nano = NanoUsb(arduino_port)
-        self.thread_id = thread_id
-        self.thread_counter = thread_counter
-        self.listen = self.Nano.recv()
-        self.sensor_pin = 20
-        self.GPIO = GPIO
-        self.GPIO.setmode(self.GPIO.BCM)
-        self.GPIO.setup(self.sensor_pin,self.GPIO.IN, pull_up_down=self.GPIO.PUD_UP)
-        self.GPIO.add_event_detect(self.sensor_pin, self.GPIO.FALLING,
-                                   callback=self.sensor_interrupt_callback(), bouncetime=50)
-        # Decide callback function (handles interrupt)
-        # If several sensor messages within 50ms, ignore (due to the "lag" of reality)
-
-        # When connected to arduino, move tray to init position
-        self.Nano.write("init")
-        state = False
-        while state:
-            # Waits until Arduino returns complete string
-            self.listen = self.Nano.recv()
-            if state is False:
-                self.listen = self.Nano.recv()
-                time.sleep(0.1)
-                if self.listen == "complete_init":
-                    state = True
-
-    def signal_handler(self, sig, frame):
-        GPIO.cleanup()
-        sys.exit(0)
-
-    def sensor_interrupt_callback(self):
-        # TODO Do Some Interrupt Code Here
-        raise EmergencyInterruptException()
-
-    def run(self):
-        """
-        Main thread loop function
-        :return:
-        """
-        while True:
-            self.listen = self.Nano.recv()
-            # Some Way To Get Buffer Comm That Plants Handed Off
-            self.Nano.write("function_1")
-            # Tell Arduino to execute function 1
-            self.listen = self.Nano.recv()
-            state = False
-            # Waits for function 1 to be finished
-            while state is False:
-                if self.listen == "complete_1":
-                    state = True
-                time.sleep(0.1)
-                self.listen = self.Nano.recv()
 
 
 class NanoUsb:
@@ -116,13 +48,122 @@ class NanoUsb:
         if self.serial_nano.in_waiting > 0:
             return True
 
-    def write(self, data):
+    def write(self, one, two, three, four):
+        data = one+two+three+four
         self.serial_nano.write(data)
 
     def recv(self):
         data = self.serial_nano.readline()
-        return data.decode('Ascii')
+        return data     #.decode('Ascii')
+
+
+class EmergencyInterruptException(Exception):
+    print("Emergency Sensor Triggered")
+    pass
+
+
+class BufferThread(NanoUsb):
+    """
+    This class deals with overall workings and logics for the buffer system
+    Call this class to create one thread for one buffer tray system
+    Sends the "init" string to Arduino tied to thread
+    Arduino handling
+    """
+    def __init__(self, arduino_port):
+        super().__init__(arduino_port)
+        """
+        :param arduino_port:
+        """
+
+        self.write_array = 0
+        self.read_array = 0
+        self.tray_ready = False
+        self.tray_ready_position = None
+        self.move_tray_init()
+        """
+        Below: Old Variables
+        """
+        self.GPIO = GPIO
+        self.GPIO.setmode(self.GPIO.BCM)
+        self.GPIO.setup(self.sensor_pin, self.GPIO.IN, pull_up_down=self.GPIO.PUD_UP)
+        # Setup the thread sensor interrupts to appropriate sensor pin
+        self.GPIO.add_event_detect(self.sensor_pin, self.GPIO.BOTH,
+                                   callback=self.sensor_interrupt_callback(), bouncetime=50)
+        self.logging = logging
+        self.logging.basicConfig(level=logging.DEBUG, filename="thread_logger.log",
+                                 filemode="w", format='%(name)s - %(levelname)s - %(message)s '
+                                                      '-%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+
+        self.logging.debug("Started high level buffer thread")
+
+    def move_tray_init(self):
+        # TODO replace with correct ints
+        self.write(1, 1, 0, 0)
+        read_array = self.read()
+        self.tray_ready_position = read_array[0]
+        self.tray_ready = True
+
+    def read(self):
+        self.read_array = self.recv()
+        read_array_old = self.read_array
+        return_array = []
+        while read_array_old == self.read_array:
+            self.read_array = self.recv()
+            if self.read_array == read_array_old:
+                time.sleep(0.05)
+            for i in self.read_array:
+                return_array.append(i)
+            return return_array
+
+    def is_ready(self):
+        """
+        Callable function that will tell you if a tray is ready or not,
+        :return: True | False & None
+        The function returns true if a tray is ready to receive plants
+        and an int (1 or 2) that corresponds to the position of the tray.
+        When False a None type object is the second return parameter.
+        """
+        tray_ready = self.tray_ready
+        tray_position = self.tray_ready_position
+        if tray_ready is False:
+            tray_position = None
+        return tray_ready, tray_position
+
+    # def signal_handler(self, sig, frame):
+    #     GPIO.cleanup()
+    #     sys.exit(0)
+    #
+    # def sensor_interrupt_callback(self):
+    #     print("Sensor Triggered")
+    #     self.Nano.write("SensorReset")
+    #
+    #     # TODO After performing system reset, send a wait to gripper/gantry
+    #     # TODO After an entire rerun of the tray has been performed, stop wait
+    #     raise EmergencyInterruptException()
+    #
+    # def run(self):
+    #     """
+    #     Main thread loop function
+    #     :return:
+    #     """
+    #     while True:
+    #         self.listen = self.Nano.recv()
+    #         # Some Way To Get Gripper That Plants Handed Off
+    #         self.Nano.write("function_1")
+    #         # Tell Arduino to execute function 1
+    #         self.listen = self.Nano.recv()
+    #         state = False
+    #         # Waits for function 1 to be finished
+    #         while state is False:
+    #             if self.listen == "complete_1":
+    #                 state = True
+    #             time.sleep(0.1)
+    #             self.listen = self.Nano.recv()
+    #
+
+
+
 
 if __name__ == '__main__':
-    thread_one = BufferThread("/dev/ttyUSB0", "upper tray", 1)
+    thread_one = BufferThread("/dev/ttyUSB0", "tray 1", 1)
     thread_one.start()
