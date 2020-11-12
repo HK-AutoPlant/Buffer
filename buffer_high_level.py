dae3b731d1c358fcaece0499c3c8a29e2ae3dd8f
Oskar Nordström <oskar.norden.kth@gmail.com>
2020-11-12 17:57:58 +0100

    Changed to Filips usbCommunication class from SC.py in Autoplant1/Gripper...

--- buffer_high_level.py
+++ buffer_high_level.py
@@ -7,6 +7,7 @@ import sys
 import threading
 import RPi.GPIO as GPIO
 import logging
+from SC import usbCommunication
 """
 HK2020 Autoplant 1 - Buffer Subsystem
 This is the high level software main file for the 2020 Mechatronics HK - Autoplant 1
@@ -21,44 +22,50 @@ https://roboticsbackend.com/raspberry-pi-gpio-interrupts-tutorial/
 """
 
 
-class NanoUsb:
-    """
-    This class creates base communication methods between Rpi and Arduino Nano
-    read/write methods using typical API write/recv
-    Needs more functions!
-    """
-    def __init__(self, port=None, baud=115200):
-        """
-        Opens Serial connection via USB from
-        """
-        if port is None:
-            port = "/dev/ttyUSB0"
-        try:
-            self.serial_nano = serial.Serial(str(port), baud)  # Example, can be ttyUSB0/1 etc
-        except(ValueError, self.serial_nano.SerialException) as e:
-            print(e)
-
-    def serial_waiting(self, nano):
-        """
-        Waiting Function that waits and then decodes a message
-        Waits until there's a communication waiting on the BUS
-        :return: True
-        True is only returned when there's more than 0 bytes on the serial connection to be read
-        """
-        if self.serial_nano.in_waiting > 0:
-            return True
-
-    def write(self, one, two, three):
-        data = one+two+three
-        self.serial_nano.write(data)
-
-    def recv(self):
-        data = self.serial_nano.readline()
-        return data     #.decode('Ascii')
+# class NanoUsb(object):
+#     """
+#     This class creates base communication methods between Rpi and Arduino Nano
+#     read/write methods using typical API write/recv
+#     Needs more functions!
+#     """
+#     def __init__(self, port=""):
+#         """
+#         Opens Serial connection via USB from
+#         """
+#         baud = 115200
+#         if port == "":
+#             port = "/dev/ttyUSB0"
+#         try:
+#             self.serial_nano = serial.Serial(str(port), baudrate=baud, timeout=0.01)  # Example, can be ttyUSB0/1 etc
+#         except ValueError as e:
+#             pass
+#         if port == "/dev/ttyUSB0":
+#             self.tray_id = 1
+#         elif port == "/dev/ttyUSB1":
+#             self.tray_id = 2
+#         else:
+#             self.tray_id = 1
+# 
+#     def serial_waiting(self, nano):
+#         """
+#         Waiting Function that waits and then decodes a message
+#         Waits until there's a communication waiting on the BUS
+#         :return: True
+#         True is only returned when there's more than 0 bytes on the serial connection to be read
+#         """
+#         if self.serial_nano.in_waiting > 0:
+#             return True
+# 
+#     def write(self, two, three):
+#         data = self.tray_id*100+10*two+three
+#         self.serial_nano.sendMessage(data)
+# 
+#     def recv(self):
+#         data = self.serial_nano.readline()
+#         return data  # .decode('Ascii')
 
 
 class EmergencyInterruptException(Exception):
-    print("Emergency Sensor Triggered")
     pass
 
 
@@ -66,7 +73,7 @@ class NoTrayReadyException(Exception):
     pass
 
 
-class BufferControl(NanoUsb):
+class BufferControl(usbCommunication):
     """
     This class deals with overall workings and logics for the buffer system
     Call this class to create one thread for one buffer tray system(?)
@@ -74,17 +81,17 @@ class BufferControl(NanoUsb):
     If both trays are ready, use tray 1
     """
     def __init__(self, arduino_port):
-        super().__init__(arduino_port)
+        super().__init__(arduino_port, 115200)
         """
         :param arduino_port:
         """
 
-        self.write_array = 0
+        self.sendMessage_array = 0
         self.read_array = 0
         self.tray_ready = False
         self.tray_position = 0
+        self.tray_id = 1
         # 0 is initial position, 1 means tray is in position to drop plant from cup 1
-        self.move_tray_init()
         self.move_ready = False
         # When True, a tray has reached the restock zone and is in waiting state
         self.nano_return_int = 111
@@ -99,22 +106,25 @@ class BufferControl(NanoUsb):
         self.GPIO.setmode(self.GPIO.BCM)
         self.GPIO.setup(self.sensor_pin, self.GPIO.IN, pull_up_down=self.GPIO.PUD_UP)
         # Setup the thread sensor interrupts to appropriate sensor pin
-        self.GPIO.add_event_detect(self.sensor_pin, self.GPIO.BOTH,
-                                   callback=self.sensor_interrupt_callback(), bouncetime=50)
+        # self.GPIO.add_event_detect(self.sensor_pin, self.GPIO.BOTH,
+        #                            callback=self.sensor_interrupt_callback(), bouncetime=50)
         self.logging = logging
         self.logging.basicConfig(level=logging.DEBUG, filename="thread_logger.log",
                                  filemode="w", format='%(name)s - %(levelname)s - %(message)s '
                                                       '-%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
         self.logging.debug("Started high level buffer thread")
+        self.move_tray_init()
+        self.sendMessage("111")
+        # self.sendMessage(1,1)
 
-    def move_tray_init(self):
+    def move_tray_init(self, input_var=True):
         # TODO replace with correct ints
-        self.write(1, 0, 0)
+        self.sendMessage("100")
         # function to move tray to restock position
         self.read()
         self.tray_position = 0
         # Assigns tray position variable to init position
-        self.tray_ready = True
+        self.tray_ready = input_var
         # tray_ready variable is True, (for is_ready function call)
 
     def state_machine(self):
@@ -140,69 +150,69 @@ class BufferControl(NanoUsb):
                 self.logging.warning("5 Plants were not found after restock, "
                                      "%d Plants were found in tray" % counter)
             if state == 2:
-                self.move_tray_to_first_drop()
+                self.move_tray_state_one()
                 self.tray_position = self.check_position()
-                if (self.tray_position < 800) and (self.tray_position > 700) and (self.plant_in_cup[0] == 1):
-                    self.open_tray()
-                    self.tray_position = self.check_position()
-
-            if state == 4:
-                self.move_tray_one_step()
+                # if (self.tray_position < 800) and (self.tray_position > 700) and (self.plant_in_cup[0] == 1):
                 self.open_tray()
                 self.tray_position = self.check_position()
-                if (self.tray_position < 600) and (self.tray_position > 500):
+
+            if state == 4:
+                self.move_tray_state_two()
+                self.open_tray()
+                # self.tray_position = self.check_position()
+                # if (self.tray_position < 600) and (self.tray_position > 500):
+                self.check_plants()
+                while self.plant_in_cup[1] == 1:
                     self.check_plants()
-                    while self.plant_in_cup[1] == 1:
-                        self.check_plants()
-                        if self.plant_in_cup[1] == 0:
-                            state = 5
-                        elif self.plant_in_cup[1] == 1:
-                            self.open_tray()
+                    if self.plant_in_cup[1] == 0:
+                        state = 5
+                    elif self.plant_in_cup[1] == 1:
+                        self.open_tray()
                 else:
                     # TODO Write to arduino to move to position
                     pass
             if state == 5:
-                self.move_tray_one_step()
+                self.move_tray_state_three()
                 self.open_tray()
                 self.tray_position = self.check_position()
-                if (self.tray_position < 500) and (self.tray_position > 400):
+                # if (self.tray_position < 500) and (self.tray_position > 400):
+                self.check_plants()
+                while self.plant_in_cup[2] == 1:
                     self.check_plants()
-                    while self.plant_in_cup[2] == 1:
-                        self.check_plants()
-                        if self.plant_in_cup[2] == 0:
-                            state = 6
-                        elif self.plant_in_cup[2] == 1:
-                            self.open_tray()
+                    if self.plant_in_cup[2] == 0:
+                        state = 6
+                    elif self.plant_in_cup[2] == 1:
+                        self.open_tray()
                 else:
                     # TODO Write to arduino to move to position 3
                     pass
             if state == 6:
-                self.move_tray_one_step()
+                self.move_tray_state_four()
                 self.open_tray()
                 self.tray_position = self.check_position()
-                if (self.tray_position < 400) and (self.tray_position > 300):
+                # if (self.tray_position < 400) and (self.tray_position > 300):
+                self.check_plants()
+                while self.plant_in_cup[3] == 1:
                     self.check_plants()
-                    while self.plant_in_cup[3] == 1:
-                        self.check_plants()
-                        if self.plant_in_cup[3] == 0:
-                            state = 7
-                        elif self.plant_in_cup[3] == 1:
-                            self.open_tray()
+                    if self.plant_in_cup[3] == 0:
+                        state = 7
+                    elif self.plant_in_cup[3] == 1:
+                        self.open_tray()
                 else:
                     # TODO Write to arduino to move to position 4
                     pass
             if state == 7:
-                self.move_tray_one_step()
+                self.move_tray_state_five()
                 self.open_tray()
                 self.tray_position = self.check_position()
-                if (self.tray_position < 300) and (self.tray_position > 200):
+                # if (self.tray_position < 300) and (self.tray_position > 200):
+                self.check_plants()
+                while self.plant_in_cup[4] == 1:
                     self.check_plants()
-                    while self.plant_in_cup[4] == 1:
-                        self.check_plants()
-                        if self.plant_in_cup[4] == 0:
-                            state = 8
-                        elif self.plant_in_cup[4] == 1:
-                            self.open_tray()
+                    if self.plant_in_cup[4] == 0:
+                        state = 8
+                    elif self.plant_in_cup[4] == 1:
+                        self.open_tray()
                 else:
                     # TODO Write to arduino to move to position 5
                     pass
@@ -212,59 +222,104 @@ class BufferControl(NanoUsb):
                 self.move_tray_init()
 
     def check_plants(self):
-        self.write(1, 2, 3)
+        """Writes 06 to arduino
+        Reads out header + 5 digits
+        If message header is 6, stores plant in cup as 1 (in cup) | 0 (not in cup)
+        :return: array 5 spots
+        """
+        self.sendMessage("106")
         output_array = []
-        data = self.recv()
+        data = self.read()
         for i in range(len(data)):
-            output_array.append(data(i))
-        self.plant_in_cup = output_array[2:]
+            output_array.append(data)
+        if output_array[0] == 6:
+            output_array = output_array[1:]
+        return output_array
+
+    def check_position(self):
+        self.sendMessage(107)
+        output_array = []
+        data = self.readMessage()
+        for i in range(len(data)):
+            output_array.append(data)
+        if output_array[0] == 6:
+            output_array = output_array[1:]
+        return output_array
 
     def move_tray_to_end(self):
-        self.write(1,2,3)
+        self.move_tray_init(False)
 
     def open_tray(self):
-        self.write(1,2,3)
+        #self.sendMessage(1,2,3)
+        #
+        self.logging.debug("There is no motor for this finished")
 
-    def move_tray_one_step(self):
-        self.write(1,2,3)
+    def move_tray_state_one(self):
+        """
+        Moves a tray to first drop off position one
+        :return: 
+        """
+        self.sendMessage("111")
         waiting_for_finish = True
         while waiting_for_finish:
             waiting_for_finish = self.read()
 
-    def move_tray_to_start(self):
-        self.write(1,2,3)
+    def move_tray_state_two(self):
+        """
+        Moves a tray to first drop off position one
+        :return: 
+        """
+        self.sendMessage("112")
+        waiting_for_finish = True
+        while waiting_for_finish:
+            waiting_for_finish = self.read()
 
-    def move_tray_to_first_drop(self):
-        self.write(1,2,3)
+    def move_tray_state_three(self):
+        """
+        Moves a tray to first drop off position one
+        :return: 
+        """
+        self.sendMessage("112")
+        waiting_for_finish = True
+        while waiting_for_finish:
+            waiting_for_finish = self.read()
+
+    def move_tray_state_four(self):
+        """
+        Moves a tray to first drop off position one
+        :return: 
+        """
+        self.sendMessage("114")
+        waiting_for_finish = True
+        while waiting_for_finish:
+            waiting_for_finish = self.readMessage()
+
+    def move_tray_state_five(self):
+        """
+        Moves a tray to first drop off position one
+        :return: 
+        """
+        self.sendMessage("115")
+        waiting_for_finish = True
+        while waiting_for_finish:
+            waiting_for_finish = self.readMessage()
+
+    def move_tray_to_start(self):
+        self.sendMessage("110")
 
     def start_tray_two(self):
         # This Function should move tray two from drop-off zone and back
         pass
 
     def read(self):
-        waiting = True
-        while waiting:
-            read = self.recv()
+        while True:
+            read = self.readMessage()
             time.sleep(0.1)
             if read == self.nano_return_int:
-                waiting = False
+                break
         return read
 
-    def write_finished(self):
-        waiting = True
-        while waiting:
-            read = self.recv()
-            time.sleep(0.1)
-            if read == 111:
-                waiting = True
-
-    def check_position(self):
-        self.write(1,2,3)
-        data = self.read()
-        position_mm = data[3:]
-        return position_mm
-
-    def trees_dropped(self, tray_number):
+    def trees_dropped(self, tray_number=None):
         self.tray_ready = False
 
     def is_ready(self):
@@ -277,10 +332,6 @@ class BufferControl(NanoUsb):
         """
         return self.tray_ready, self.tray_position_restock
 
-    # def signal_handler(self, sig, frame):
-    #     GPIO.cleanup()
-    #     sys.exit(0)
-    #
     def sensor_interrupt_callback(self):
         print("Sensor Triggered")
     #
@@ -290,4 +341,6 @@ class BufferControl(NanoUsb):
 
 
 if __name__ == '__main__':
-    Buffer_Control = BufferControl("/dev/ttyUSB0")
+    Buffer_Control = BufferControl("/dev/ttyACM0")
+    # Start state machine
+    Buffer_Control.state_machine()
